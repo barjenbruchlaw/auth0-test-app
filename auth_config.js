865ad7dd2e5722b6d5f1c459b6880744c229adfa
barjenbruchlaw <brian@kcpropertylaw.com>
2022-08-30 12:15:12 -0500

    first commit with errors VIDEO 52:50

---
+++ auth_config.js
@@ -0,0 +1,8 @@
+// auth_config.js
+
+const config = {
+    domain: "dev-6wzd5x7x.us.auth0.com",
+    clientId: "JQiwZLFFQqsMIRO9fq2LioszXYc8aZqq"
+  };
+  
+  export default config;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,88 @@
+<!-- Application -->
+<script>
+  import auth from "../authService.js"
+  import TaskItem from "../components/TaskItem.svelte"
+  let newTask
+  import { user, isAuthenticated, popupOpen } from "./store.js"
+
+  function login() {
+    auth.loginWithPopup(auth0Client)
+  }
+  function logout() {
+    auth.logout(auth0Client)
+  }
+
+  function addItem() {
+    let newTaskObject = {
+      id: genRandom(),
+      description: newTask,
+      completed: false,
+      user: $user.email,
+    }
+
+    console.log(newTaskObject)
+
+    let updatedTasks = [...$tasks, newTaskObject]
+
+    tasks.set(updatedTasks)
+
+    newTask = ""
+  }
+
+  function genRandom(length = 7) {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var result = ""
+    for (var i = length; i > 0; --i)
+      result += chars[Math.round(Math.random() * (chars.length - 1))]
+    return result
+  }
+</script>
+
+<main>
+  {#if !$isAuthenticated}
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-10 offset-md-1">
+          <div class="jumbotron">
+            <h1 class="display-4">Task Management made Easy!</h1>
+            <p class="lead">Instructions</p>
+            <ul>
+              <li>Login to start &#128272;</li>
+              <li>Create Tasks &#128221;</li>
+              <li>Tick off completed tasks &#9989;</li>
+            </ul>
+            <a
+              class="btn btn-primary btn-lg mr-auto ml-auto"
+              href="/#"
+              role="button"
+              on:click={login}>Log In</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  {:else}
+    <div class="container" id="main-application">
+      <div class="row">
+        <div class="col-md-6">
+          <ul class="list-group">
+            {#each $user_tasks as item (item.id)}
+              <TaskItem task={item} />
+            {/each}
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <input
+            class="form-control"
+            bind:value={newTask}
+            placeholder="Enter New Task"
+          />
+          <br />
+          <button type="button" class="btn btn-primary" on:click={addItem}>
+            Add Task
+          </button>
+        </div>
+      </div>
+    </div>
+  {/if}
+</main>
